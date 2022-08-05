87f27c0a3868ce86db181b49c5747147abda957c
PBillingsby <philbillingsby@gmail.com>
2022-08-05 12:59:44 -0400

    new commit

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import Arweave from 'arweave';
+const arweave = Arweave.init({
+  host: 'arweave.net',
+  port: 443,
+  protocol: 'https'
+});
+
+const query = (address: string) => {
+  const vouchQuery = {
+    query: `
+    query {
+      transactions(
+        tags:{name:"Vouch-For", values:[${address}]}
+      ) {
+        edges {
+          node {
+            id
+            tags {
+              name 
+              value 
+            }
+          }
+        }
+      }
+    }   
+`}
+  return vouchQuery;
+}
+
+export const isVouched = async (address: string) => {
+  const vouchQuery = query(address);
+  try {
+    const results = await arweave.api.post(`graphql`, vouchQuery)
+      .catch((err: any) => {
+        console.error('GraphQL query failed');
+        throw new Error(err);
+      });
+    const edges = results.data.data.transactions.edges;
+    return edges.length > 1 ? true : false;
+  }
+  catch (error) {
+    console.log(error);
+    return error
+  }
+}
+
