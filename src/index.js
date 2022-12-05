21d64440e22ac92feb43345a3ce5d5f5d12ca42e
PBillingsby <philbillingsby@gmail.com>
2022-12-05 10:50:57 -0500

    update isVouched

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { WarpFactory } from 'warp-contracts'
+
+const warp = WarpFactory.forMainnet();
+
+const contract = warp.contract("_z0ch80z_daDUFqC9jHjfOL8nekJcok4ZRkE_UesYsk").connect("use_wallet").setEvaluationOptions({
+  allowBigInt: true
+});
+
+export const isVouched = async (address) => {
+  const { cachedValue } = await contract.readState();
+  return cachedValue.state.vouched[address]
+}
+
+
---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+import { isVouched } from '../src/index.js';
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+
+test('returns true for verified address', async () => {
+  const res = await isVouched("UZ1YsJa8yJrw8yynYzhaAikqD1uuMu9gi9u7Ia_Eja8")
+  const obj = Object.keys(res).length;
+  console.log(obj)
+  assert.is.not(obj, obj === 0)
+});
+
+test('returns false for unverified address', async () => {
+  const res = await isVouched("_JiUqSPY2y8jpoDM4b56iD5embeJph0y_oIexbCEA5Q")
+  assert.is(res, undefined)
+});
+
+test.run();
